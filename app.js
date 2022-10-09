73e4a09aae66c0997e23c1b6aa915f32a4661eed
Alex <rengarsanj@gmail.com>
2022-10-09 13:33:18 -0800

    09.10.2022_StableVersionForLocal

--- app.js
+++ app.js
@@ -16,56 +16,43 @@ app.use(express.static(__dirname + '/'));
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
-
+client.setConfig({apiKey: "26d8ce00606059f75a3bf961e8150de0-us13",  server: "us13",});
 
 app.post("/", function(req, res) {
-	client.setConfig({apiKey: "26d8ce00606059f75a3bf961e8150de0-us13",  server: "us13",});
 	const subscribingUser = {firstName: req.body.fName, lastName: req.body.lName, email: req.body.email};
 	
-	
-
-
 	const run = async () => {
-		const response = await client.lists.addListMember("bd928c4dce", {
-			email_address: subscribingUser.email,
-			status: "subscribed",
-			merge_fields: {
-				FNAME: subscribingUser.firstName,
-				LNAME: subscribingUser.lastName
-        	}
-		});
-
-		res.sendFile(__dirname + "/success.html");
-
-		
-
-		console.log(response.status)
-		// console.log(response);
-
-		
+		try {
+			const response = await client.lists.addListMember("bd928c4dce", {
+				email_address: subscribingUser.email,
+				status: "subscribed",
+				merge_fields: {
+					FNAME: subscribingUser.firstName,
+					LNAME: subscribingUser.lastName
+				}
+			});
+			console.log(response.status)
+			res.sendFile(__dirname + "/success.html");
+		} catch (err) {
+			console.log(err.status);
+			res.sendFile(__dirname + "/failure.html");
+		}
 	};
-
 	run();
-	
-	
-		
-	
-
-
 });
 
 
+app.post("/failure", function(req, res) {
+	res.redirect("/")
+})
+
+app.post("/success", function(req, res) {
+	res.redirect("/")
+})
 
 
-
-
-app.get("/", function(req, res) {
-	res.sendFile(__dirname + "/index.html");
-});
-
-
-
-app.listen(3000, function() {
+// 3000 -> process.env.PORT for heroku servers 
+app.listen(process.env.PORT || 3000, function() {
 	console.log("Server has just started on port 3000");
 });
 
